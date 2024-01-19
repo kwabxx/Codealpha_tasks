ca13ad257650825ca66132e0737fd70464603410
kwabxx <93738804+kwabxx@users.noreply.github.com>
2024-01-19 12:23:40 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    colors:{
+      kay: "#4203d2",
+      yebs:"#90bafc",
+      white: "#fff",
+      rgb: "#e1ffff4d",
+      teal: "#CCFBF1",
+      yel: "#ffff76",
+      yll: "#e1ff4d",
+      sff: "#6b4dff"
+    },
+    fontFamily: {
+      owu: "Quicksand', sans-serif;"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
